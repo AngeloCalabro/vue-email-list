6705b7db5b424952656bc81a0169e63f0eaa22b9
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-08 15:13:40 +0100

    genera lista quando hai la lista completa

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            emailRandom: []
+            emailRandom: [],
+            listaNum: 1
         }
     },
     methods: {
         callApi() {
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < this.listaNum; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
                     console.log(res.data.response);
                     let email = res.data.response;
                     this.emailRandom.push(email);
                 })
             }
+            console.log(this.listaNum);
+            this.emailRandom = [];
         }
     },
     created() {
